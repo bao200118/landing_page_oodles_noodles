9a0769eac5f3f5943dd88de6b2164a17d4fefecb
bao200118 <60221952+bao200118@users.noreply.github.com>
2024-04-29 22:48:26 +0700

    add second screen landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import ScreenOne from './component/ScreenOne/ScreenOne.tsx'
+import ScreenTwo from './component/ScreenTwo/ScreenTwo.tsx'
 
 function App() {
   return (
     <>
       <div className="screen">
         <ScreenOne />
+      </div>
+      <div className="screen">
+        <ScreenTwo />
       </div>
     </>
   )
--- src/component/ScreenTwo/ScreenTwo.tsx
+++ src/component/ScreenTwo/ScreenTwo.tsx
@@ -1,11 +1,18 @@
 import './ScreenTwo.css'
-import Button from '../Button/Button.tsx'
-import illustrationFirst from '../../assets/images/first_screen_image.png'
 
 function ScreenTwo() {
   return (
-    <div className="screen-one">
-      <div className={}></div>
+    <div className="screen-two">
+      <div className="text-container">
+        <div className="title">
+          It's been a huge season,<br />
+          and we all<span className="purple"> deserve to unwind</span><br />
+          and connect as team members.
+        </div>
+        <div className="invitation">
+          Join us for noodles, karaoke, and all around good vibes!
+        </div>
+      </div>
     </div>
   )
 }
