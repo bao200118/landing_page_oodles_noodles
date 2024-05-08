75ae12d617510e5be722472cea70a5ebb55d797a
bao200118 <60221952+bao200118@users.noreply.github.com>
2024-05-08 08:22:14 +0700

    Revert "optimize responsive screen one"
    This reverts commit 15b7ff782b0fe71ac08719d3c3d3f44c769f6ec8.

--- src/component/ScreenFour/ScreenFour.tsx
+++ src/component/ScreenFour/ScreenFour.tsx
@@ -3,11 +3,11 @@ import MenuBox from '../MenuBox/MenuBox.tsx'
 
 function ScreenFour() {
   return (
-    <div className="menu-box-content">
+    <>
       <MenuBox title={`Gogyo\n Menu`} content={`$20 per person.\nPre-order your food to receive it on time.`} buttonLabel="View menu" />
-      <MenuBox title={`Karaoke\n playlist`} content="Please pre-select a song to play so we can prepare a playlist in advance." buttonLabel="Add a song" />
+      <MenuBox title="Karaoke playlist" content="Please pre-select a song to play so we can prepare a playlist in advance." buttonLabel="Add a song" />
       <MenuBox title={`Team\n t-shirts`} content={`Each shirt costs $40.\nThese are optional and must be ordered a week ahead.`} buttonLabel="Order a shirt" />
-    </div>
+    </>
   )
 }
 
